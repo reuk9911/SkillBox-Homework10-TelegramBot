2dd7c809a2711477f681478b707c3cf777f6ad0c
Vitaliy Maltsev <reuk9911@yandex.ru>
2022-01-29 17:14:08 +0400

    task10 youtube

---
+++ Homework10/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot;
+using System.Net;
+using System.Net.Http;
+//using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace Homework10
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary
+    public partial class MainWindow : Window
+    {
+
+        ObservableCollection<TelegramUser> Users;
+        TelegramBotClient bot;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Users = new ObservableCollection<TelegramUser>();
+
+            userList.ItemsSource = Users; //установка источника данных
+
+            string token = "2129445094:AAEGP9HqkpXcvIRV5HY5Fy_Fbh508bKI-rk";
+            bot = new TelegramBotClient(token);
+
+            //указываем допустимые протоколы
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | 
+                SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | 
+                SecurityProtocolType.Tls12;
+            
+            
+            var me = bot.GetMeAsync().Result;
+            Debug.WriteLine($"Bot_Id: {me.Id} \nBot_Name: {me.FirstName} ");
+
+            bot.OnMessage += delegate (object sender, Telegram.Bot.Args.MessageEventArgs e)
+            {
+                string msg = $"{DateTime.Now}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+                File.AppendAllText("data.log", $"{msg}\n");
+                Debug.WriteLine(msg);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    var person = new TelegramUser(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                    if (!Users.Contains(person)) Users.Add(person);
+                    Users[Users.IndexOf(person)].AddMessage($"{person.Nick}: {e.Message.Text}");
+                });
+            };
+            bot.StartReceiving();
+            btnSendMsg.Click += delegate {SendMsg(); };
+            txtBxSendMsg.KeyDown += (s, e) => { if (e.Key == Key.Return) { SendMsg(); } };
+        }
+
+        /// <summary>
+        /// Отправка сообщения
+        /// </summary>
+        public void SendMsg()
+        {
+            var concreteUser = Users[Users.IndexOf(userList.SelectedItem as TelegramUser)];
+            string responseMsg = $"Support: {txtBxSendMsg.Text}";
+            concreteUser.Messages.Add(responseMsg);
+            
+            bot.SendTextMessageAsync(concreteUser.Id, txtBxSendMsg.Text);
+            string logText = $"{DateTime.Now}: >> {concreteUser.Nick} {concreteUser.Id} {responseMsg}";
+            File.AppendAllText("data.log", logText);
+            txtBxSendMsg.Text = String.Empty;
+        }
+    }
+}
---
+++ Homework10/TelegramUser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using System.IO;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Homework10
+{
+    public class TelegramUser: INotifyPropertyChanged, IEquatable<TelegramUser>
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="Nickname">Имя пользователя</param>
+        /// <param name="ChatId">Id чата</param>
+        public TelegramUser(string Nickname, long ChatId)
+        {
+            this.nick = Nickname;
+            this.id = ChatId;
+            Messages = new ObservableCollection<string>();
+        }
+
+        private string nick;
+
+        public string Nick
+        {
+            get { return this.nick; }
+            set
+            {
+                this.nick = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.nick)));
+            }
+        }
+
+        private long id;
+        public long Id
+        {
+            get { return this.id; }
+            set { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.id))); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged; // оповещение внешних агентов
+        public bool Equals(TelegramUser other) => other.Id == this.id;
+
+        /// <summary>
+        /// Все сообщения
+        /// </summary>
+        public ObservableCollection<string> Messages { get; set; }
+
+        public void AddMessage(string Text) => Messages.Add(Text);
+    }
+}
